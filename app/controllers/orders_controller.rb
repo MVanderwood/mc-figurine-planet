9a75f07577c5ab75f62c7d1d6315abca7cadfc33
MVanderwood <m.b.vanderwood@gmail.com>
2015-11-25 18:53:50 -0600

    added user_id functionality to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,13 @@ class OrdersController < ApplicationController
   def create
     order = Order.create({
       quantity: params[:quantity],
-      figurine_id: params[:figurine_id]
+      figurine_id: params[:figurine_id],
+      user_id: current_user.id
       })
     redirect_to "/orders/#{order.id}"
   end
 
   def show
-
+    @order = Order.find_by(id: params[:id])
   end
 end
